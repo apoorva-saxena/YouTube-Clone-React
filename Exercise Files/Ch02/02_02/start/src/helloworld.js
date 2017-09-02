06ec919a530b8f06b9e11dcccf8531629bffff2d
Apoorva Saxena <apsaxena24@gmail.com>
2017-09-02 13:02:50 +0100

    build

---
+++ Exercise Files/Ch02/02_02/start/src/helloworld.js
@@ -0,0 +1,11 @@
+const HelloWorld = React.createClass({
+    		render: function() {
+    			return(
+    				<div>
+    					<h1> Hello World </h1>
+    					<p> My name is Apoorva </p>
+    				</div>
+    			)
+    		}
+    	})
+React.render(<HelloWorld />, document.body)
