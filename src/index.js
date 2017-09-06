3c5ff39c017e6396cc0a16c94538f51e63e6f601
Apoorva Saxena <apsaxena24@gmail.com>
2017-09-07 01:10:25 +0530

    getting 0th element

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+
 const API_KEY = 'AIzaSyBn7kTORO8keZ7Lnw_F9EIRBgE3CE-7ci4';
-
-
 
 class App extends Component {
 	constructor (props) {
@@ -22,7 +22,8 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos}/>
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
